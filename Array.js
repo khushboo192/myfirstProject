a2d96252715f17684959bf0d8d719eb3158aa661
khushboo192 <khushbookajal570@gmail.com>
2026-07-09 09:24:34 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,54 @@
+//array in js
+
+ 1. let arr = [10,20,30,40,50];
+ console.log (arr);
+//2.
+let arr = [khushboo]
+ console.log(arr);
+
+//3. basic program
+let arr = [10, 20, 30, 40]
+let sum = 0
+sum = sum + arr;
+console.log( "sum =", sum);
+
+//4. element delete
+let arr = [10, 20, 30, 40, 50];
+arr.splice(2, 1);
+console.log(arr);
+
+5.//element add
+let arr = [40,50,60,70];
+arr.splice(2,0,30);
+console.log(arr);
+
+6.// push 
+let arr = [10, 20, 30];
+arr.push(40);
+console.log(arr);
+
+//7. push
+let arr = [10, 20, 30, 40, 50, 60, 70, 80];
+arr.push(90);
+console.log(arr);
+
+
+8.// pop 
+let arr = [10,20,30]
+arr.pop();
+console.log(arr);
+
+9.//pop
+let arr = [40,50,60,70,80];
+arr.pop(80);
+console.log(arr);
+
+10.//unshift
+let arr = [40, 50, 60, 70, 80];
+arr.unshift(70);
+console.log(arr);
+
+
+// 11. join program 
+let arr = [ "A", "B","C" ];
+console.log(arr.join("-"));
